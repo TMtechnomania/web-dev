575333f21fd2ba2cb2531e2c04507fee5b4574bd
Kartikey Tiwari <tiwari.kartik3002@gmail.com>
2022-09-06 15:02:05 +0530

    android loading test

--- projects/fake-windows-update/js/script.js
+++ projects/fake-windows-update/js/script.js
@@ -114,3 +114,74 @@ function Windows(){
         }
     }
 }
+
+function Android(){
+    let defaultValue = 7500;
+    let timestamp = document.getElementById("timestamp").value;
+    if (timestamp > 0)
+    {
+        defaultValue = timestamp * 1250;
+    }
+    if (defaultValue === 7500)
+    {
+        console.log('default value: 7500');
+    }
+    else{
+        console.log(defaultValue);
+    }
+    let fscreen = document.getElementById("Android");
+    fscreen.style.zIndex = "25";
+    fscreen.style.opacity = "1";
+    document.body.webkitRequestFullScreen();
+    var count=0;
+    var stage=1;
+    var stage2=3;
+    var counter=setInterval(timer, defaultValue); //1000 will  run it every 1 second
+    var ref="Configuring updates";
+
+    function timer()
+    {
+        count=count+1;
+        if (count <= 0)
+        {
+            clearInterval(counter);
+            return;
+        }
+
+        document.getElementById("timer").innerHTML=count +'%';
+        
+        if (count > 29)
+        {
+            count=count+2;
+        }
+        
+        if (count > 68)
+        {
+            ref="Installing Updates";
+            count=count+3;
+            document.getElementById("ref").innerHTML=ref;
+        }
+
+        if (count === 100)
+        {
+            clearInterval(counter);
+            let fscreen = document.getElementById("Android");
+            fscreen.style.zIndex = "-25";
+            fscreen.style.opacity = "0";
+            const cancellFullScreen = document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen;
+            cancellFullScreen.call(document);
+            count = 0;
+        }
+
+        if (count > 99)
+        {
+            clearInterval(counter);
+            let fscreen = document.getElementById("Android");
+            fscreen.style.zIndex = "-25";
+            fscreen.style.opacity = "0";
+            const cancellFullScreen = document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen;
+            cancellFullScreen.call(document);
+            count = 0;
+        }
+    }
+}
